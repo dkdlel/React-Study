965103a29cf09222165426143e4bab513c370f7e
JeJn <leejj1082@icloud.com>
2021-03-11 15:25:07 +0900

    #12장 README

--- chap12_Immutable-Maintenance-use-immer/src/App.js
+++ chap12_Immutable-Maintenance-use-immer/src/App.js
@@ -190,11 +190,11 @@ const App = () => {
   const onChange = useCallback(e => {
     const { name, value } = e.target;
     setForm(
-      produce(form, draft => {
+      produce(draft => {
         draft[name] = value;
       })
     )
-  }, [form])
+  }, [])
 
   // form 등록을 위한 함수
   const onSubmit = useCallback(e => {
@@ -207,7 +207,7 @@ const App = () => {
 
     // array에 새 항목 등록
     setData(
-      produce(data, draft => {
+      produce(draft => {
         draft.array.push(info);
       })
     )
@@ -218,17 +218,17 @@ const App = () => {
       username: ''
     })
     nextId.current += 1;
-  }, [data, form.name, form.username])
+  }, [form.name, form.username])
 
 
   // 항목을 삭제하는 함수
   const onRemove = useCallback(id => {
     setData(
-      produce(data, draft => {
+      produce(draft => {
         draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
       })
     )
-  }, [data])
+  }, [])
 
   return (
     <div>
