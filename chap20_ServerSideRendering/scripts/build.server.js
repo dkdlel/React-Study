144e73e6cfdf8bbd2aacb9ad15bd11e22d1bcbd0
JeJn <leejj1082@icloud.com>
2021-03-30 17:27:16 +0900

    #20장 기본적인 서버 사이드 렌더링

---
+++ chap20_ServerSideRendering/scripts/build.server.js
@@ -0,0 +1,34 @@
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
+
+process.on('unhandleRejection', err => {
+    throw err;
+});
+
+require('../config/env');
+const fs = require('fs-extra');
+const webpack = require('webpack');
+const config = require('../config/webpack.config.server');
+const paths = require('../config/paths');
+
+function build() {
+    console.log('Creating server build...');
+    fs.emptyDirSync(paths.ssrBuild);
+    let compiler = webpack(config);
+    return new Promise((resolve, reject) => {
+        compiler.run((err, status) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log(status.toString());
+        });
+    });
+}
+
+build();
+// node scripts/build.server.js
+// node dist/server.js
+/* pakage.json-scripts 서버 명령어 추가 */
+// yarn build:server
+// yarn start:server
--- chap20_ServerSideRendering/src/index.server.js
+++ chap20_ServerSideRendering/src/index.server.js
@@ -1,10 +1,89 @@
 /* 20.3.1 서버 사이드 렌더링용 엔트리 만들기 */
+// import React from 'react';
+// import ReactDomServer from 'react-dom/server';
+
+// // 서버에서 리액트 컴포넌터를 렌더링 하는 함수
+// const html = ReactDomServer.renderToString(
+//     <div>Hello Server Side Rendering!</div>
+// );
+
+// console.log(html);
+
+/* 20.3.4 서버 코드 작성하기 */
 import React from 'react';
 import ReactDomServer from 'react-dom/server';
+import express from 'express';
+import { StaticRouter } from 'react-router-dom';
+import App from './App';
+/* 20.3.5 정적 파일 제공하기 */
+import path from 'path';
+import fs from 'fs';
 
-// 서버에서 리액트 컴포넌터를 렌더링 하는 함수
-const html = ReactDomServer.renderToString(
-    <div>Hello Server Side Rendering!</div>
+
+/* 20.3.5 정적 파일 제공하기 */
+// asset-manifest.json에서 파일 경로들을 조회함
+const manifest = JSON.parse(
+    fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf-8')
 );
 
-console.log(html);
+const chunks = Object.keys(manifest.files)
+    .filter(key => /chunk\.js$/.exec(key)) // chunk.js로 끝나는 키를 찾아서
+    .map(key => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환하고
+    .join(''); // 합침
+
+function createPage(root) {
+    return `
+    <!DOCTYPE html>
+    <html lang="kr">
+        <head>
+            <meta charset="utf-8" />
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+            <meta name="theme-color" content="#000000" />
+            <title>React App</title>
+            <link href="${manifest.files['main.css']}" rel="stylesheet" />
+        </head>
+        <body>
+            <noscript>You need to enable Javascript to run this app.</noscript>
+            <div id="root">${root}</div>
+            <script src="${manifest.files['runtime~main.js']}"></script>
+            ${chunks}
+            <script src="${manifest.files['main.js']}"></script>
+        </body>
+    </html>
+    `;
+}
+/* ------------ */
+
+const app = express();
+
+// 서버 사이드 렌더링을 처리할 핸들러 함수
+const serverRender = (req, res, next) => {
+    // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해 줌
+    const context = {};
+    const jsx = (
+        <StaticRouter location={req.url} context={context}>
+            <App />
+        </StaticRouter>
+    );
+    const root = ReactDomServer.renderToString(jsx); // 렌더링 하고
+    // res.send(root); // 클라이언트에게 결과물을 응답
+    /* 20.3.5 정적 파일 제공하기 */
+    res.send(createPage(root)); // 결과물을 응답
+};
+
+/* 20.3.4 서버 코드 작성하기 */
+// app.use(serverRender);
+
+/* 20.3.5 정적 파일 제공하기 */
+const serve = express.static(path.resolve('./build'), {
+    index: false // "/" 경로에서 index.html을 보여주지 않도록 설정
+});
+
+app.use(serve); // 순서가 중요함. serverRender 전에 위치해야 함
+app.use(serverRender);
+
+// 5000 포트로 서버를 가동
+app.listen(5000, () => {
+    console.log('Running on http://localhost:5000');
+});
